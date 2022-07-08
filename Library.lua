6c7726ae0548c8661b176d66ace3f89a6d30518a
Cyph <96255759+Cyphrous@users.noreply.github.com>
2022-07-08 23:24:55 +0800

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -17,8 +17,8 @@ local themes = {
 	Background = Color3.fromRGB(255, 77, 109), 
 	Glow = Color3.fromRGB(0, 0, 0), 
 	Accent = Color3.fromRGB(164, 19, 60), 
-	LightContrast = Color3.fromRGB(128, 15, 47), 
-	DarkContrast = Color3.fromRGB(89, 13, 34),  
+	LightContrast = Color3.fromRGB(89, 13, 34), 
+	DarkContrast = Color3.fromRGB(255, 77, 109),  
 	TextColor = Color3.fromRGB(255, 179, 193)
 }
 
